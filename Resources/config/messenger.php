ecad989171abc0ff2e510a458b34c6984fdd6286
kingstar <admin@baks.dev>
2024-03-14 16:41:43 +0300

    Baks Development

--- Resources/config/messenger.php
+++ Resources/config/messenger.php
@@ -44,6 +44,8 @@ return static function(FrameworkConfig $framework) {
     ;
 
     $messenger->transport('failed-wildberries-manufacture')
-        ->dsn('%env(MESSENGER_TRANSPORT_DSN)%');
+        ->dsn('%env(MESSENGER_TRANSPORT_DSN)%')
+        ->options(['queue_name' => 'failed-wildberries-manufacture'])
+    ;
 
 };
